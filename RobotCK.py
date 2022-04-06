67123bea289d8ab26bfd7e694e56af8b727a4998
CKAyano <triton6564685@gmail.com>
2022-04-06 18:35:24 +0800

    plot updated

--- RobotCK.py
+++ RobotCK.py
@@ -4,6 +4,7 @@ import copy
 from Package.NelderMeadSimplex import simplex
 from enum import Enum, auto
 from mpl_toolkits import mplot3d
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
 
@@ -306,6 +307,41 @@ class Robot:
 
 
 class Plot:
+    # PRE_TRANS =
+    @staticmethod
+    def getCynByAxis(redius=1, heightStart=0, heightEnd=5, offset=[0, 0, 0], devision=20, mainAxis="z"):
+
+        mainAxis = mainAxis.lower()
+
+        theta = np.linspace(0, 2 * np.pi, devision)
+        cx = np.array([redius * np.cos(theta)])
+        cz = np.array([heightStart, heightEnd])
+        cx, cz = np.meshgrid(cx, cz)
+        cy = np.array([redius * np.sin(theta)] * 2)
+
+        if mainAxis == "z":
+            return offset[0] + cx, offset[1] + cy, offset[2] + cz
+        elif mainAxis == "y":
+            return offset[0] + cx, offset[1] + cz, offset[2] + cy
+        elif mainAxis == "x":
+            return offset[0] + cz, offset[1] + cy, offset[2] + cx
+        else:
+            raise ValueError("'x', 'y' or 'z' PLZ")
+
+    def drawCylinder(self):
+
+        cx, cy, cz = self.getCynByAxis(
+            offset=[1, 2, 3], devision=40, mainAxis="x", heightEnd=5, heightStart=0, redius=0.5
+        )
+
+        fig = plt.figure(figsize=(11, 10))
+        ax = plt.axes(projection="3d")
+        ax.plot_surface(cx, cy, cz, rstride=1, cstride=1, linewidth=0, alpha=0.25)
+        ax.set_xlim(-5, 5)
+        ax.set_ylim(-5, 5)
+        ax.set_zlim(0, 10)
+        plt.show()
+
     @staticmethod
     def plot_robot(trans_list: List[Trans], save_path: Optional[str] = None) -> None:
         fig = plt.figure()
